0b8a6e6c2558afa12763f0694bca61913e7766fa
myopicOracle <184602237+myopicOracle@users.noreply.github.com>
2024-12-28 11:05:15 -0800

    Initialize the geolocation API, create map, add tiles, and add marker

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -10,3 +10,25 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+// basic geolocation API - takes 2 callbacks
+// navigator.geolocation.getCurrentPosition(function(){},function(){})
+
+navigator.geolocation.getCurrentPosition(
+    function(position){
+      console.log("Location found")  
+    },
+    function(position){
+        console.log("Unable to find location")
+    })
+
+// Leaflet sample code to 1. create map; 2. add tiles; 3. add marker
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+    .openPopup();
