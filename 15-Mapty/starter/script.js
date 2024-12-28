5552b979e31a36b5f5a24cc64a9af8b30e4e9257
myopicOracle <184602237+myopicOracle@users.noreply.github.com>
2024-12-28 14:25:28 -0800

    Refactor for Leaflet code which adds map, tiles, and marker to coords

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -17,11 +17,24 @@ const inputElevation = document.querySelector('.form__input--elevation');
 if (navigator.geolocation)
     navigator.geolocation.getCurrentPosition(
         function(position){
-            const {latitude} = position.coords;
-            const {longitude} = position.coords;
-            console.log(latitude)
-            console.log(longitude)
-            console.log(`https://www.google.com/maps/@${latitude},@${longitude}`)
+            const { latitude } = position.coords;
+            const { longitude } = position.coords;
+            // console.log(latitude)
+            // console.log(longitude)
+            // console.log(`https://www.google.com/maps/@${latitude},@${longitude}`)
+
+            const coords = [ latitude, longitude ]
+
+            const map = L.map('map').setView(coords, 13); // 'map' is id of map element in html
+
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(map);
+            
+            L.marker(coords).addTo(map) // replaced hard-codes with "coords" array
+                .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+                .openPopup();
+
         },
         function(){
             console.log("Unable to find location")
@@ -29,13 +42,19 @@ if (navigator.geolocation)
     )
 
 
+
+
+
+
+
+
 // Leaflet sample code to 1. create map; 2. add tiles; 3. add marker
-// const map = L.map('map').setView([latitude, longitude], 13);
+    // const map = L.map('map').setView([latitude, longitude], 13);
 
-// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-// }).addTo(map);
+    // L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    //     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    // }).addTo(map);
 
-// L.marker([51.5, -0.09]).addTo(map)
-//     .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-//     .openPopup();
+    // L.marker([51.5, -0.09]).addTo(map)
+    //     .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+    //     .openPopup();
