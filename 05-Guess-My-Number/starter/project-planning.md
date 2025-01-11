3cbe4a5b04acf1e7626244ea113b3d08faad0d90
myopicOracle <184602237+myopicOracle@users.noreply.github.com>
2025-01-10 19:57:46 -0800

    05 - Guess My Number: First working version complete, see features below
    Features Flowchart:
    - generate a random number + store internally
    - on Check button click:
        1. check if user input is equal to stored random number
    - if YES:
        1. display msg
        2. change '?'
        3. change background color to green
        4. set Highscore = Score
    - if NO:
        1. alert "Wrong!"
        2. decrement Score
        3. change display to "Try again"
        4. clear Number input box
    - on Again button click:
        1. reset game

--- 05-Guess-My-Number/starter/project-planning.md
+++ 05-Guess-My-Number/starter/project-planning.md
@@ -2,16 +2,17 @@ Flowchart & Pseudocode
 
 Features Flowchart:
 - generate a random number + store internally
-- on button click:
+- on Check button click:
     1. check if user input is equal to stored random number
 - if YES:
     1. display msg
     2. change '?' 
     3. change background color to green
     4. set Highscore = Score
-    5. reset game 
 - if NO:
     1. alert "Wrong!"
     2. decrement Score
     3. change display to "Try again"
     4. clear Number input box
+- on Again button click:
+    1. reset game
--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,71 @@
 'use strict';
+
+// grab DOM elements
+const wrapper = document.querySelector("body")
+const headerText = document.querySelector("h1")
+const againButton = document.querySelector("button.again")
+const displayNumber = document.querySelector(".number")
+const userInput = document.querySelector(".guess")
+const checkButton = document.querySelector("button.check")
+const alertHighOrLow = document.querySelector(".message")
+const currentScore = document.querySelector(".score")
+const highScore = document.querySelector(".highscore")
+
+// needed since currentScore is string in DOM
+let scoreAsNumber = Number(currentScore.textContent)
+
+// 'secretNumber' var immediately available in global scope
+let secretNumber = (function() {
+    return Math.floor(Math.random() * 20) + 1
+})();
+
+// necessary since implementing make bg red if wrong guess
+const _inputHandler = function() {
+    headerText.textContent = "You Can Do It!"
+    wrapper.style.backgroundColor = "#222"
+    displayNumber.textContent = "?"
+    userInput.value = ""
+}
+
+// break this apart to be less cumbersome?
+const _checkGuess = function() {
+    console.log(+userInput.value)
+    console.log(secretNumber)
+    if (+userInput.value === secretNumber) {
+        wrapper.style.backgroundColor = "limegreen"
+        headerText.textContent = `
+        Woohoo! You Win!!! \n
+        The Secret Number Was:
+        `
+        displayNumber.textContent = `${secretNumber}`
+        alertHighOrLow.textContent = "You are correct!"
+        if (currentScore.textContent > highScore.textContent) highScore.textContent = currentScore.textContent 
+    } else if (+userInput.value !== secretNumber) {
+        wrapper.style.backgroundColor = "darkred"
+        headerText.textContent = "Oops... Try Again!"
+        displayNumber.textContent = ":("
+        if (+userInput.value > secretNumber) {
+            alertHighOrLow.textContent = "Your guess is too HIGH!"
+        } else if (+userInput.value < secretNumber) {
+            alertHighOrLow.textContent = "Your guess is too LOW!"
+        }
+        --scoreAsNumber
+        currentScore.textContent = scoreAsNumber
+        userInput.value = ""
+    }
+}
+
+const _playAgain = function() {
+    headerText.textContent = "Guess My Number!"
+    displayNumber.textContent = "?"
+    userInput.value = ""
+    alertHighOrLow.textContent = "Start guessing..."
+    currentScore.textContent = "20"
+    scoreAsNumber = 20
+    wrapper.style.backgroundColor = "#222"
+    secretNumber = Math.floor(Math.random() * 20) + 1
+}
+
+userInput.addEventListener("click", _inputHandler)
+checkButton.addEventListener("click", _checkGuess)
+againButton.addEventListener("click", _playAgain)
