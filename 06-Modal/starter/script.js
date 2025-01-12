1ae366a977a0a99dd1e9fc388c25bd64bbbf842b
myopicOracle <184602237+myopicOracle@users.noreply.github.com>
2025-01-11 16:10:37 -0800

    06-Modal-Window | Self-Guided 2nd Working Version -- Features Finished

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,38 +1,45 @@
 'use strict';
 
+// DOM els needed for modal window 
 const modal = document.querySelector(".modal")
-
 const buttonShowModal = Array.from(document.querySelectorAll(".show-modal"))
-
 const buttonCloseModal = document.querySelector(".close-modal")
 
+// DOM els needed to show overlay
+const overlay = document.querySelector(".overlay")
+
+// show modal window on click of any 3 top buttons
 buttonShowModal.forEach(item => item.addEventListener("click", (e) => {
     modal.classList.remove("hidden")
+    overlay.classList.remove("hidden")
     e.target.style.backgroundColor = "gold"
 }))
 
+// close modal window on click "X"
 buttonCloseModal.addEventListener("click", () => {
     modal.classList.add("hidden")
+    overlay.classList.add("hidden")
     buttonShowModal.forEach(item => item.style.backgroundColor = "white")
 })
 
-// trying out toggle -- not part of final solution
 
+// trying out toggle -- not part of official solution
 const wrapper = document.querySelector("body")
-
 const buttonToggle = document.createElement("button")
-buttonToggle.setAttribute("class", "toggle")
-buttonToggle.textContent = "Toggle Modal"
-buttonToggle.style.cssText = "width: 100px; height: 50px; padding: 10px; border: 4px solid blue; color: darkorange; background-color: lightgreen; align-self: end;"
-wrapper.appendChild(buttonToggle)
+    buttonToggle.setAttribute("class", "toggle")
+    buttonToggle.textContent = "Toggle Modal"
+    buttonToggle.style.cssText = "width: 120px; height: 50px; margin-bottom: 30px; border: 4px solid blue; color: darkorange; font-weight: 900; background-color: lightgreen; align-self: end;"
+    wrapper.appendChild(buttonToggle)
 
 buttonToggle.addEventListener("click", (e) => {
     modal.classList.toggle("hidden")
+    overlay.classList.toggle("hidden")
     if (modal.classList[1] === "hidden") {
         e.target.style.backgroundColor = "lightgreen"
         e.target.style.color = "darkorange"
         e.target.style.fontWeight = "400"
     } else {
+        e.target.style.zIndex = "15"
         e.target.style.backgroundColor = "magenta"
         e.target.style.color = "white"
         e.target.style.fontWeight = "900"
